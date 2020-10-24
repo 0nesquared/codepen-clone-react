74d57679fa271903d4ce899e89ff6a46c473378e
dimstar <aman120498@gmail.com>
2020-10-24 23:59:04 +0530

    Render the code in iframe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 
 function App() {
+  // states to store the user-entered codes in the editors, and the srcDoc for the iframe
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  /* Any time our code changes, we want to update our srcDoc after a small delay (say 1 sec)
+     so that the effect is that whenever we are done typing or takig a small pause, only then 
+     does the srcDoc update, and cause the iframe to render the updated code */
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`<html>
+                  <body>${html}</body>
+                  <style>${css}</style>
+                  <script>${js}</script>
+                </html>`);
+    }, 1000);
+    /* the timeout cleanup is necessary so that whenever the code changes, we don't end up 
+    creating a new timeout function again without cleaning the old one */
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
   return (
     <>
@@ -30,10 +48,11 @@ function App() {
           onChange={setJs}
         />
       </div>
-      {/* The preview region */}
+      {/* The preview region, that would render our source-doc defined above */}
       <div className="panel">
         <iframe
           title="output"
+          srcDoc={srcDoc}
           sandbox="allow-scripts"
           frameBorder="0"
           width="100%"
